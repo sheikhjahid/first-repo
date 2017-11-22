3eb05348d23f7a36ae142ca84208bad774c71f1c
jahid <jahid@itobuz.com>
2017-11-22 15:13:34 +0530

    app.js was updated today-jahid

--- app.js
+++ app.js
@@ -7,22 +7,26 @@ add.onclick=function(){
 
 }
 
-var todos = []
+var todos = [];
 var todosFromStorage;
 function loadTodos(){
 	if(localStorage.getItem('todos')){
-		let todosFromStorage = JSON.parse(localStorage.getItem('todos'))
-		console.log(todosFromStorage)
-	}
-
- document.write(JSON.parse(localStorage.getItem('todos')));
+		let todosFromStorage = JSON.parse(localStorage.getItem('todos'));
+		console.log(todosFromStorage);
+    //document.write(JSON.parse(localStorage.getItem('todos',)));
+    //document.write(JSON.parse(localStorage.getItem('id')));
+    const LiTemplate=`<li>${JSON.parse(localStorage.getItem('todos'))}</li>`;
+    document.write(LiTemplate);
+    const LiTemplate1=`<li>${JSON.parse(localStorage.getItem('id'))}</li>`;
+    document.write(LiTemplate1);
+    console.log(todos);
+    console.log(id);
+	} 
 }
-
-
- //console.log(todosFromStorage);
-
+//console.log(todosFromStorage);
+//document.write(console.log(todosFromStorage));
  
-var main_product_id;
+/*var main_product_id;
 var items=JSON.parse(localStorage.getItem("todos")); 
        items=items.filter(function(item) {
                    if(main_product_id!=todos.id)
@@ -31,16 +35,23 @@ var items=JSON.parse(localStorage.getItem("todos"));
                    }
        });
    localStorage.setItem("todos", JSON.stringify(todos));
+*/
+
+
+//const LiTemplate = `<li>${name}</li>`
 
 
 loadTodos()
 
+var id=[]
 function addLi(targetUi)
 {
+  var temp_id=+new Date()  
 	var inputText=document.getElementById("text").value;
 	todos.push(inputText);
+  id.push(temp_id);
 	li=document.createElement("li"),
-	textNode=document.createTextNode(inputText+" "),
+	textNode=document.createTextNode("TEXT = "+inputText+",  id  =  "+temp_id),
 	removeButton=document.createElement("button");
 	document.getElementById("text").value=" ";
 
@@ -60,9 +71,12 @@ function addLi(targetUi)
      targetUi.appendChild(li);
 
      console.log(todos)
-     localStorage.setItem('todos', JSON.stringify(todos))
+     localStorage.setItem('todos',JSON.stringify(todos))
+     localStorage.setItem('id', JSON.stringify(id))
 		
 }
+
+
 function delete_item(item)
      {
      	var parent=item.parentElement;
@@ -75,11 +89,4 @@ function delete_item(item)
      {
      	ul.innerHTML=" "; 
      }
-
-     
-
-
-
-
-
 //alert("Hi");
